9bfb80a425f4fa679cad648a66df9872f7c34f7d
KwokCheungYuk <49836764+KwokCheungYuk@users.noreply.github.com>
2019-05-11 18:18:00 +0800

    Update fptree.cpp

--- Programming-FPTree/src/fptree.cpp
+++ Programming-FPTree/src/fptree.cpp
@@ -55,6 +55,29 @@ int InnerNode::findIndex(const Key& k) {
 // WARNING: can not insert when it has no entry
 void InnerNode::insertNonFull(const Key& k, Node* const& node) {
     // TODO
+    int inserIndex = 0;
+    bool isBreak = false;
+    for(int i = 0; i < nKeys; i ++){
+        if(k < keys[i]){
+            inserIndex = i;
+            isBreak = true;
+            break;
+        }
+    }
+    if(isBreak == false){
+        childrens[nChild] = node;
+        keys[nKeys] = k;
+    }
+    else{
+        for(int i = nKeys; i > index; i --){
+            Keys[i] = Keys[i - 1];
+            childrens[i + 1] = childrens[i];
+        }
+        Keys[inserIndex] = k;
+        childrens[inserIndex + 1] = node;
+    }
+    nChild ++;
+    nKeys ++;
 }
 
 // insert func
